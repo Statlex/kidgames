17c5e61adb70a2c88f31c765056bf4188d05c7c5
Dmitri Turovtsov <Dmitri1981>
2013-12-04 14:36:18 +0300

    Sound was fixed.

--- KidMath/web/js/player.js
+++ KidMath/web/js/player.js
@@ -5,6 +5,9 @@
 
 	win.player = {
 		musicOn: dataStorage.getItem('music-on') || 'yes',
+		currentSrc: '',
+		currentMedia: '',
+		currentMediaStatus: 4, // is stopped
 		play: function (src) {
 
 //			console.log(src);
@@ -13,23 +16,22 @@
 			if (this.musicOn === 'no') {
 				return;
 			}
-			this.stop();
-			try {
-				this.currentMedia = new Media(src, this.onSuccess, this.onError);
-				try {
-					this.currentMedia.play();
-				} catch (e) {
-				}
-			} catch (e) {
+
+			if (this.currentMediaStatus <= 2 ) {
+				return;
 			}
+
+			if (this.currentSrc !== src) {
+				this.currentMedia = new Media(src, this.onSuccess, this.onError, this.onStatus);
+				this.currentSrc = src;
+			}
+
+			this.currentMedia.play();
 
 		},
 		stop: function(){
-			try {
-				if (this.currentMedia) {
-					this.currentMedia.pause();
-				}
-			} catch (e) {
+			if (this.currentMedia && this.currentMedia.stop) {
+				this.currentMedia.stop();
 			}
 		},
 		onSuccess: function () {
@@ -37,6 +39,9 @@
 		},
 		onError: function (error) {
 			//alert(error.code + ' - ' + error.message);
+		},
+		onStatus: function(status) {
+			player.currentMediaStatus = status;
 		},
 		setMusic: function(on) {
 			var value = on ? 'yes' : 'no';
