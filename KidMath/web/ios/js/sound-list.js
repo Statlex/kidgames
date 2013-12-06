7f15755573a84e3e0b96c666f000b28cde5daa78
Pavel Sychykau <pavel.sychukau@conmio.com>
2013-12-06 16:24:08 +0300

    Re-create ios application

--- KidMath/web/ios/js/sound-list.js
+++ KidMath/web/ios/js/sound-list.js
@@ -1,19 +1,1 @@
-(function (win) {
-
-	"use strict";
-	/*global window, document, console, alert */
-
-    var path = window.location.pathname;
-    var phoneGapPath = path.substring(0, path.lastIndexOf('/') + 1);
-
-	var pathPrefix ='sound/';
-
-	win.soundList = {
-		goodAnswer: pathPrefix + 'good-answer.mp3',
-		click: pathPrefix + 'click.mp3',
-		endSection: pathPrefix + 'end-section.mp3',
-		badAnswer: pathPrefix + 'bad-answer.mp3',
-		swipe: pathPrefix + 'swipe.mp3'
-	}
-
-}(window));
+﻿(function (win) {	"use strict";	/*global window, document, console, alert */	var pathPrefix = 'sound/';	win.soundList = {		goodAnswer: pathPrefix + 'good-answer.mp3',		click: pathPrefix + 'click.mp3',		endSection: pathPrefix + 'end-section.mp3',		badAnswer: pathPrefix + 'bad-answer.mp3',		swipe: pathPrefix + 'swipe.mp3'	}}(window));
