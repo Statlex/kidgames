2a9073ca1806af9067090fe1e3ce67a8296593ff
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2013-12-07 01:50:09 +0300

    KidGame - in progress.

--- KidGame/web/js/find-number.js
+++ KidGame/web/js/find-number.js
@@ -4,43 +4,73 @@
 	/*global window, document */
 
 	win.findNumber = {
-		availableNumbers: $.createSimpleArray(0, 20), // [0..20]
+		init: function() {
+			win.addEventListener('resize', this.setBlocksSize.bind(this), false);
+			this.template = $('.js-template.find-number').innerHTML;
+			this.template = viewer.template(this.template);
+		},
+		availableNumbers: $.createSimpleArray(0, 19), // [0..19]
+		questions: [],
 		showLevel: function() {
-			var answer = $.shuffle(Object.create(this.availableNumbers))[10];
 
 			// if difficult == 1 -> show message + voice
 			// if difficult == 2 -> voice only
 			// if difficult == 3 -> voice only + change number positions
 
-			var blocks = $$('.js-number-for-find');
+			this.questions = Object.create(this.availableNumbers);
+			this.answer = $.shuffle(Object.create(this.availableNumbers))[10];
 
-			blocks.forEach(function(node){
-				node.onclick = function(){
-					$.addClass(this, 'disable');
-					this.onclick = function(){};
-				}
-			});
+			console.log(this.answer);
 
 			if (info.difficult === 1) {
-				ui.message.show(lang[info.lang].find + ': ' + answer);
-
-
+				//ui.message.show(lang[info.lang].find + ': ' + answer);
 			}
 
 			if (info.difficult === 2) {
-
+				this.questions = $.shuffle(this.questions);
 			}
 
 			if (info.difficult === 3) {
-
+				this.questions = $.shuffle(this.questions);
 			}
 
+			$('#wrapper').innerHTML = this.template({});
+
+			var that = this;
+			var blocks = $$('#wrapper .js-number-for-find');
+			blocks.forEach(function(node){
+				node.onclick = function() {
+					if (parseInt(node.getAttribute('number')) === that.answer) {
+						that.showLevel();
+					} else {
+						$.addClass(this, 'disable');
+						this.onclick = function(){};
+					}
+				}
+			});
+
+			this.setBlocksSize();
+
+		},
+		setBlocksSize: function() {
+			var blocks = $$('#wrapper .js-number-for-find span');
+			blocks.forEach(function(node){
+				node.style.fontSize = 0;
+				node.style.lineHeight = 0;
+			});
+
+			var height = (blocks[0] && blocks[0].clientHeight) || 0;
+			blocks.forEach(function(node){
+				node.style.lineHeight = height + 'px';
+				node.style.fontSize = height * 0.8 + 'px';
+			});
 
 		}
 
 
 
-	}
+	};
 
+	win.addEventListener('load', findNumber.init.bind(findNumber), false);
 
 }(window));
--- KidGame/web/js/main.js
+++ KidGame/web/js/main.js
@@ -7,11 +7,8 @@
 		handleEvent: function() {
 			// init viewer
 			this.wrapper = $('#wrapper');
-			this.setWrapperSize();
 			viewer.wrapper = this.wrapper;
 			viewer.show('.title-page');
-			win.addEventListener('resize', this.setWrapperSize.bind(this), false);
-
 		},
 		test: function() {
 			console.log(this);
@@ -20,7 +17,7 @@
 
 			switch (info.section) {
 				case 'find-number':
-					$$('#wrapper .button').forEach(function(button){
+					$$('#wrapper .main-button').forEach(function(button){
 						var attribute = button.getAttribute('onclick');
 						attribute += 'viewer.show(".find-number");';
 						button.setAttribute('onclick', attribute);
@@ -29,12 +26,7 @@
 
 			}
 
-		},
-		setWrapperSize: function() {
-			this.wrapper.style.width = info.screen.width() + 'px';
-			this.wrapper.style.height = info.screen.height() + 'px';
 		}
-
 	};
 
 	win.addEventListener('load', win.main, false);
