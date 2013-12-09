204ff97585ee9e229036ad6376a304c95e32d3cd
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2013-12-09 23:08:11 +0300

    Add some for languages.

--- KidGame/web/android/js/lang/lang-de.js
+++ KidGame/web/android/js/lang/lang-de.js
@@ -13,6 +13,7 @@
 		easy: 'de easy',
 		normal: 'de normal',
 		hard: 'de hard',
+		alphabetButton: 'abc',
 		alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ü", "ß"], // + "ss" -> ß
 		find: 'de find'
 	}
--- KidGame/web/android/js/lang/lang-en.js
+++ KidGame/web/android/js/lang/lang-en.js
@@ -13,8 +13,8 @@
 		easy: 'easy',
 		normal: 'normal',
 		hard: 'hard',
+		alphabetButton: 'abc',
 		alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-		alphabetSoundMap: [1.3, 2.1, 3, 3.9, 4.8, 5.5, 6.2, 7, 7.8, 8.6, 9.4, 10.2, 11, 11.7, 12.6, 13.2, 13.9, 14.7, 15.3, 16.2, 16.9, 17.6, 18.5, 19.5, 20.2, 21.1, 22],
 		find: 'find'
 	}
 
