e712d1af341643742758c358fb8282612103bcfd
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2013-12-24 15:49:32 +0300

    KGLRT - started.

---
+++ KidLgicReThink/web/android/js/services/finder.js
@@ -0,0 +1,74 @@
+(function (win, doc) {
+
+	/*global window, document, Array, RegExp */
+
+	win.$ = function (selector, context) {
+		return (context || doc).querySelector(selector);
+	};
+
+	win.$$ = function (selector, context) {
+		return win.$.toArray((context || doc).querySelectorAll(selector));
+	};
+
+	win.$.hasClass = function (node, className) {
+		var re = new RegExp('^' + className + ' | ' + className + ' | ' + className + '$|^' + className + '$', 'g');
+		return re.test(node.className);
+	};
+
+	win.$.removeClass = function (node, className) {
+		var re = new RegExp('^' + className + ' | ' + className + ' | ' + className + '$|^' + className + '$', 'g');
+		var nodeClass = node.className;
+		if (re.test(nodeClass)) {
+			node.className = nodeClass.replace(re, ' ').trim();
+		}
+	};
+
+	win.$.addClass = function (node, className) {
+		if (!win.$.hasClass(node, className)) {
+			node.className += node.className ? ' ' + className : className;
+		}
+	};
+
+	win.$.assortFunction = function() {
+		return Math.random() - 0.5;
+	};
+
+	win.$.shuffle = function (arr) {
+		arr.forEach(function (value, index, array) {
+			array.sort(win.$.assortFunction);
+		});
+		return arr;
+	};
+
+	win.$.createSimpleArray = function(begin, end) {
+		var arr = [], i;
+		for (i = begin; i <= end; i++) {
+			arr.push(i);
+		}
+		return arr;
+	};
+
+	win.$.hexToRgb = function(hex) {
+
+		//#FCA -> #FFCCAA
+		if (hex.length <= 4) {
+			hex = hex.replace(/(\w)(\w)(\w)/gi, '$1$1$2$2$3$3');
+		}
+
+		var rgb = hex.match(/\w{2}/gi);
+		return parseInt(rgb[0], 16) + ',' + parseInt(rgb[1], 16) + ',' + parseInt(rgb[2], 16);
+	};
+
+	win.$.toArray = function(list) {
+		return Array.prototype.slice.call(list);
+	};
+
+	win.$.html = function(node, html) {
+		if (html !== undefined) {
+			node.innerHTML = html;
+			return true;
+		}
+		return node.innerHTML;
+	}
+
+}(window, document));
