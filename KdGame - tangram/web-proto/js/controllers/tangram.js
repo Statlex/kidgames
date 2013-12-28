1204e1d8a434434cc28f69a15c966b4638800f3a
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2013-12-28 19:26:34 +0300

    GTP - in progress.

--- KdGame - tangram/web-proto/js/controllers/tangram.js
+++ KdGame - tangram/web-proto/js/controllers/tangram.js
@@ -5,19 +5,84 @@
 
 	var tangram;
 	tangram = {
+		figureList: ['B3A', 'B3A', 'M3A', 'S3A', 'S3A', 'SQR', 'TRP'],
 		handleEvent: function() {
 
 		},
 		init: function() {
 
-			var wrapper = $('#wrapper');
+			this.wrapper = $('#wrapper .page');
+			this.mainImage = $('svg', this.wrapper);
+			this.mainImage.setAttribute('class', 'main-image js-main-image');
+			this.mainImageWidth = this.mainImage.getAttribute('width');
+			this.mainImageHeight = this.mainImage.getAttribute('height');
+			this.resizeMainImage(this.mainImage, 1);
+			this.createActiveFigures(1);
 
-			console.log(wrapper);
+		},
+		resizeMainImage: function(svg, q) {
+
+			var re = /\d+|\d+\.\d+/gi;
+			var svgAttributes = ['x', 'y', 'width', 'height'];
+			svgAttributes.forEach(function(attr){
+				var svgAttr = svg.getAttribute(attr);
+				svgAttr = svgAttr.match(re)[0];
+				svgAttr = svgAttr * q + 'px';
+				svg.setAttribute(attr, svgAttr);
+			});
+
+		},
+		createActiveFigures: function(q) {
+
+			var mainModel = {
+				x: 0,
+				y: 0,
+				width: this.wrapper.clientWidth,
+				height: this.wrapper.clientHeight,
+				figures: '{{figures}}'
+			};
+
+			// get svg
+			var html = viewer.templates['tangram-figures-wrapper'].html;
+			// add svg for figures to field
+			var mainSVG = viewer.template(html)(mainModel);
+
+			var figuresModel = {
+				fillColor: '0C0',
+				strokeColor: '000'
+			};
+
+			var figuresStr = '';
+
+			this.figureList.forEach(function(figureName){
+				var polygon = viewer.template(figuresCode.template);
+				// set figures color here
+
+				figuresModel.points = figuresCode[figureName];
+				polygon = polygon(figuresModel);
+				// resize figures here
+
+				figuresStr += polygon;
+			});
+
+			mainSVG = mainSVG.replace('{{figures}}', figuresStr);
+			this.wrapper.innerHTML += mainSVG;
 
 		}
 
 
 	};
+
+	var figuresCode = {
+		B3A: '50,100 0,50 50,0',
+		M3A: '35.355,70.711 35.355,0 0,35.355',
+		S3A: '0,25 25,50 25,0',
+		SQR: '0,0 35.355,0 35.355,35.355 0,35.355',
+		TRP: '0,50 0,0 25,25 25,75',
+		TRPR: '0,25 0,75 25,50 25,0',
+		template: '<polygon fill="#{{= fillColor }}" stroke="#{{= strokeColor }}" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="{{= points }}"/>'
+	};
+
 
 	win.tangram = tangram;
 
---
+++ KdGame - tangram/web-proto/js/figures/figures-1.js
@@ -0,0 +1,26 @@
+(function (win) {
+
+	"use strict";
+	/*global window, document */
+
+	win.rabbit = {
+		svg: '<?xml version="1.0" encoding="utf-8"?><svg version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="139px" height="226px" viewBox="0 0 139 226" xml:space="preserve">  <g>   <polygon fill="#BF7400" points="118.794,140.421 83.44,105.065 48.084,69.71 48.084,115.421 23.084,140.421 48.084,165.421  48.084,140.421 83.439,175.776 48.084,175.776 48.084,211.132 83.439,211.132 118.795,211.132 118.794,211.131  "/>   <polygon fill="#BF7400" points="48.084,69.709 48.084,34.354 12.729,34.354 12.729,69.709 30.406,69.709  "/>   <polygon fill="#BF7400" points="98.084,34.354 123.084,9.354 73.084,9.354 48.084,34.354 85.584,34.354  "/>  </g>  </svg>',
+		name: 'rabbit-name',
+		//figureList: ['B3A', 'B3A', 'M3A', 'S3A', 'S3A', 'SQR', 'TRP', 'TRPR'],
+		figureCoords: [
+			[65.77, 122.78, 45],
+			[101.16, 158.09, -135],
+			[84.45, 193.45, -90],
+			[56.94, 184.59, -45],
+			[35.58, 140.39, 0],
+			[30.4, 52, 0],
+			[85.58, 21.85, -90]
+		]
+
+
+
+
+	}
+
+
+}(window));
