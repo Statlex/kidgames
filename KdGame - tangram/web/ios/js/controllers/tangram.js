3556c0d16c61f1561c43dc97e4278ada4370dad9
Dmitri Turovtsov <Dmitri1981>
2014-01-08 11:03:31 +0300

    TG is started.

---
+++ KdGame - tangram/web/ios/js/controllers/tangram.js
@@ -0,0 +1,24 @@
+(function (win) {
+
+	"use strict";
+	/*global window, document, console, alert */
+
+	var tg;
+
+	tg = {
+		init: function(){
+			console.log('tangram init');
+		}
+
+
+
+	};
+
+
+
+
+
+
+	win.tangram = tg;
+
+}(window));
