c5047009d900b7b8c7b860c533adf5878ede483b
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-01-12 21:08:03 +0300

    TG - in progress.

--- KdGame - tangram/svg-in-line/fixer.js
+++ KdGame - tangram/svg-in-line/fixer.js
@@ -3,16 +3,55 @@
 	"use strict";
 	/*global window, document, console, alert */
 
+	var inLiner = {
+		handleEvent: function(){
+			this.textArea = document.querySelector('.js-text-area');
+			this.button = document.querySelector('.js-button');
+			this.button.addEventListener('click', this.createSVGLine.bind(this), false);
+		},
+		createSVGLine: function() {
+			var svgLine = this.textArea.value;
+			svgLine = (svgLine.match(/<svg[\s\S]+<\/svg>/gi))[0];
+			svgLine = svgLine.replace(/\n/gi, '').replace(/"/gi, "'").replace(/\s+/gi, ' ').replace(/\s'/gi, "'");
+
+			var tempNode = document.createElement('div');
+			tempNode.innerHTML = svgLine;
+			var elements = tempNode.querySelectorAll('*');
+			elements = Array.prototype.splice.call(elements);
+			elements.forEach(function(elem){
+
+
+
+			});
+
+
+
+
+
+
+
+
+			svgLine = 'svg:"' + svgLine + '",\n';
+			this.textArea.value = svgLine;
+		}
+
+
+	};
+
+
 
 
 	function setButton() {
-		var button = document.querySelector('.js-button');
-		var textArea = document.querySelector('.js-text-area');
 
 		function createSVGLine() {
+
 			var str = (textArea.value.match(/<svg[\s\S]+<\/svg>/gi))[0];
+
 			str = str.replace(/\n/gi, '').replace(/"/gi, "'").replace(/\s+/gi, ' ').replace(/\s'/gi, "'");
+
 			textArea.value = 'svg:"' + str + '",\n';
+
+
 		}
 
 		button.addEventListener('click', createSVGLine, false);
@@ -20,10 +59,6 @@
 	}
 
 
-	function run() {
-		setButton();
-	}
-
-	window.addEventListener('load', run, false);
+	window.addEventListener('load', inLiner, false);
 
 }());
