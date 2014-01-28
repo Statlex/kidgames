16b0e00a740471e10aefc9f6dc7f219e1dd7b163
Pavel Sychykau <pavel.sychukau@conmio.com>
2014-01-28 17:59:47 +0300

    Add iOS version

---
+++ KdGameTangram/ios/tangram/platforms/ios/www/js/figures/animal-1.js
@@ -0,0 +1,35 @@
+(function (win) {
+
+	'use strict';
+	/*global window, document */
+
+	win.categories = win.categories || {};
+
+	win.categories.animal_1 = {
+		svg:"<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='254.25px' height='250.75px' viewBox='0 0 254.25 250.75' enable-background='new 0 0 254.25 250.75' xml:space='preserve'><g> <polygon fill='#CCCCCC' points='40.395,174.249 75.75,209.604 111.105,209.604 75.75,174.249'/> <polygon fill='#CCCCCC' points='211.105,38.896 186.105,63.896 161.105,38.896 161.105,88.896 111.105,138.896 111.105,209.604 111.105,209.605 181.816,209.605 161.105,188.896 161.105,159.605 196.461,124.251 186.105,113.896 211.105,88.896'/></g></svg>",
+		name_ar: 'ar animals #1',
+		name_de: 'de animals #1',
+		name_en: 'animals #1',
+		name_es: 'es animals #1',
+		name_ru: 'животные #1',
+		name_zh: 'zh animals #1',
+		figures: [
+			{
+				svg:"<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='240.094px' height='300px' viewBox='0 0 240.094 300' enable-background='new 0 0 240.094 300' xml:space='preserve'><g> <polygon fill='#7F7F7F' points='128.259,44.612 178.259,44.612 153.259,69.612 103.26,69.612'/> <polygon fill='#7F7F7F' points='173.971,175.678 173.971,175.677 173.971,175.677 173.971,175.622 173.941,175.648 103.26,104.967 103.26,150.677 78.26,175.677 103.26,200.677 103.26,175.677 138.588,211.005 138.561,211.032 103.259,211.032 103.259,246.389 103.314,246.333 173.971,246.332'/> <polygon fill='#7F7F7F' points='103.26,69.611 103.26,69.611 67.904,69.611 67.904,104.967 103.26,104.967'/></g></svg>",
+				id: 8055156976915896
+			},
+			{
+				svg:"<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='254.25px' height='250.75px' viewBox='0 0 254.25 250.75' enable-background='new 0 0 254.25 250.75' xml:space='preserve'><g> <polygon fill='#CCCCCC' points='40.395,174.249 75.75,209.604 111.105,209.604 75.75,174.249'/> <polygon fill='#CCCCCC' points='211.105,38.896 186.105,63.896 161.105,38.896 161.105,88.896 111.105,138.896 111.105,209.604 111.105,209.605 181.816,209.605 161.105,188.896 161.105,159.605 196.461,124.251 186.105,113.896 211.105,88.896'/></g></svg>",
+				id: 7916543669998646
+			},
+			{
+				svg:"<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='240.094px' height='300px' viewBox='0 0 240.094 300' enable-background='new 0 0 240.094 300' xml:space='preserve'><g> <polygon fill='#7F7F7F' points='128.259,44.612 178.259,44.612 153.259,69.612 103.26,69.612'/> <polygon fill='#7F7F7F' points='173.971,175.678 173.971,175.677 173.971,175.677 173.971,175.622 173.941,175.648 103.26,104.967 103.26,150.677 78.26,175.677 103.26,200.677 103.26,175.677 138.588,211.005 138.561,211.032 103.259,211.032 103.259,246.389 103.314,246.333 173.971,246.332'/> <polygon fill='#7F7F7F' points='103.26,69.611 103.26,69.611 67.904,69.611 67.904,104.967 103.26,104.967'/></g></svg>",
+				id: 80551569769
+			}
+
+		]
+
+	}
+
+}(window));
+
---
+++ KdGameTangram/ios/tangram/www/js/main.js
@@ -0,0 +1,188 @@
+(function (win) {
+
+	"use strict";
+	/*global window, document */
+
+	var main = {
+
+		handleEvent: function() {
+
+			this.wrapper = $('#wrapper');
+			viewer.show('title-page');
+
+			this.uniqueTest();
+
+		},
+		setMorePage: function(){
+			var langSelectNode = $('.js-language-select', this.wrapper);
+
+			langSelectNode.addEventListener('change', function(){
+				info.set('lang', this.value, true);
+				viewer.refresh();
+			}, false);
+
+			// set external link
+			var links = $$('a', main.wrapper);
+			ui.externalLinkHandler.setLinks(links);
+
+		},
+		setSettingPage: function(){
+			var langSelectNode = $('.js-language-select', this.wrapper);
+			langSelectNode.addEventListener('change', function(){
+				info.set('lang', this.value, true);
+				viewer.refresh();
+			}, false);
+
+			var savedItems = $$('input[save]', main.wrapper);
+			savedItems.forEach(function(item){
+				item.addEventListener('change', function(){
+					var itemName = this.getAttribute('save');
+					info.set(itemName, this.checked, true);
+				}, false);
+			});
+
+		},
+		setPreview: function(wrapper) {
+			var activePreviews = $('.js-tangram-preview.active', main.wrapper);
+			$.removeClass(activePreviews, 'active');
+			$.addClass(wrapper, 'active');
+
+			var patternName = wrapper.getAttribute('pattern-name');
+			info.set('tangramPattern', patternName, true);
+		},
+		setImageColor: function() {
+			var imgs = $$('.js-category-button', main.wrapper);
+			var tempNode = document.createElement('div');
+			var pre = "background-image: url('data:image/svg+xml;utf8,";
+			var post = "');";
+			imgs.forEach(function(img){
+				tempNode.innerHTML = img.getAttribute('style').replace(pre, '').replace(post, '');
+				var polygons = $$('polygon', tempNode);
+				polygons.forEach(function(polygon){
+					polygon.setAttribute('fill', info.imageColor);
+					polygon.setAttribute('stroke', info.imageColor);
+					polygon.setAttribute('stroke-width', '1');
+					polygon.setAttribute('stroke-linecap', 'round');
+					polygon.setAttribute('stroke-linejoin', 'round');
+					polygon.setAttribute('stroke-miterlimit', '12');
+				});
+				img.setAttribute('style', pre + tempNode.innerHTML + post);
+			})
+
+		},
+		uniqueTest: function() {
+			// test for unique id in categories
+			var categories = win.categories;
+
+			var ids = [];
+
+			var testIsPassed = true;
+
+			for (var key in categories) {
+				if (categories.hasOwnProperty(key)) {
+					categories[key].figures.forEach(function(data){
+						ids.push(data.id);
+					});
+				}
+			}
+
+			console.log('---- Unique ID test is STARTED ----');
+
+			ids.forEach(function(id, index, arr){
+				if (arr.indexOf(id) !== arr.lastIndexOf(id)) {
+					console.log('double id is - ' + id);
+					testIsPassed = false;
+				}
+			});
+
+			if (testIsPassed) {
+				console.log('---- Unique ID test is PASSED ---');
+			} else {
+				console.warn('---- Unique ID test is ERROR ----');
+			}
+
+		},
+		setPreviewState: function() {
+
+			var nodes = $$('.category-wrapper', main.wrapper);
+
+			var idsData = info.get('idsData') || {};
+
+			nodes.forEach(function(node){
+				var imageId = parseInt(node.getAttribute('figure-id'));
+				if (!idsData[imageId]) {
+					return;
+				}
+				var timeNode = $('.js-timestamp', node);
+				var saveIcon = $('.js-save-icon', node);
+				var removeIcon = $('.js-remove-icon', node);
+				removeIcon.setAttribute('figure-id', imageId);
+				$.addClass(removeIcon, 'active');
+				$.addClass(saveIcon, 'saved');
+
+				removeIcon.addEventListener('click', function(e){
+					e.stopPropagation();
+					ui.confirm.show(lang[info.lang].removeTgState,
+						function(){
+							var imgId = parseInt(this.getAttribute('figure-id'));
+							// remove from LS
+							var idsData = info.get('idsData');
+							delete idsData[imgId];
+							info.set('idsData', idsData, true);
+							// remove from DB
+							dataBase.removeDataBySVGId(imgId);
+
+							// hide extra elements
+							this.style.display = 'none';
+							var timer = $('.js-timestamp', this.parentNode);
+							timer.style.display = 'none';
+							var saveIcon = $('.js-save-icon', this.parentNode);
+							$.removeClass(saveIcon, 'saved');
+
+						}.bind(this),
+						function(){
+
+						}
+					);
+
+				}, false);
+
+				if (!info.timerIsActive) {
+					return;
+				}
+				var spendTime = idsData[imageId].spendTime;
+				if (spendTime > 0) {
+					var min = Math.floor(spendTime / 60);
+					var sec = spendTime % 60;
+					sec = (sec <= 9) ? ('0' + sec) : sec;
+					timeNode.innerHTML = min + ':' + sec;
+					timeNode.style.display = 'block';
+				}
+
+			});
+
+
+
+		}
+
+	};
+
+	win.main = main;
+
+	win.addEventListener('load', main, false);
+
+}(window));
+
+(function () {
+	"use strict";
+	/*global window, document, console, alert */
+
+	window.addEventListener('load', noBodyScroll, false); // + no gesture
+	function noBodyScroll() {
+		var wrapper = $('body');
+		wrapper.addEventListener('touchmove', function(e){
+			e.preventDefault();
+		}, false);
+	}
+
+}());
