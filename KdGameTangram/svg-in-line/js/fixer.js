448fb98f7a0ea272e9ed5a4ed98ecf56abf71471
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-02-02 18:32:28 +0300

    TG - fixer.js add some functionals.

--- KdGameTangram/svg-in-line/js/fixer.js
+++ KdGameTangram/svg-in-line/js/fixer.js
@@ -8,6 +8,7 @@
 			this.textArea = document.querySelector('.js-text-area');
 			this.fileInput = document.querySelector('.js-files');
 			this.fileInput.addEventListener('change', this.handleFileSelect.bind(this), false);
+			this.imagesWrapper = document.querySelector('.js-images-wrapper');
 		},
 		handleFileSelect: function(evt) {
 
@@ -35,19 +36,60 @@
 				alert('Rectangle in ' + file.name + '!!!');
 				return '';
 			}
+
+			if (svgLine.indexOf('<g>') !== -1) {
+				console.warn('<g> in ' + file.name + '!!!');
+				svgLine = svgLine.replace( /<g>|<\/g>/gi, '');
+			}
+
 			svgLine = (svgLine.match(/<svg[\s\S]+<\/svg>/gi))[0];
 			svgLine = svgLine.replace(/id=\"\S*?\"/gi , '')
 				.replace(/id=\"\S*?\"/gi , '')
 				.replace(/baseProfile=\"\S*?\"/gi , '')
-				.replace(/\n/gi, '').replace(/"/gi, "'").replace(/\s+/gi, ' ').replace(/\s'/gi, "'");
+				.replace(/\n/gi, '')
+				.replace(/"/gi, "'")
+				.replace(/\s+/gi, ' ')
+				.replace(/\s'/gi, "'")
+				.replace(/>\s+</gi, '><')
+				.replace(/fill='.*?'/gi, "fill='#0C0'");
 
+			// add svg image to page
+			this.addImageToPage(svgLine);
 
 			svgLine = 'svg:"' + svgLine + '",\n';
 			var id = Math.random().toString().replace('0.', '').replace(/^0+/gi, '');
 			id = 'id: ' + id + '\n';
 			svgLine += id;
-			svgLine = '{\n' + svgLine + '},\n'
+			svgLine = '{\n' + svgLine + '},\n';
 			return svgLine;
+
+		},
+		addImageToPage: function(svgLine) {
+
+			var svg = svgLine;
+
+			// get dots
+			var dots = svg.match(/points='.*?'/gi).join(' ').replace(/points='|'/gi, '').split(' ');
+			dots.forEach(function(dot, index, arr){
+				var xy = dot.split(',');
+				arr[index] = {
+					x: xy[0],
+					y: xy[1]
+				}
+			});
+
+			var circleTemplate = '<circle cx="{{x}}" cy="{{y}}" r="3" fill="#00C" />';
+			var circles = '';
+			dots.forEach(function(dot){
+				circles += circleTemplate.replace('{{x}}', dot.x).replace('{{y}}', dot.y);
+			});
+
+			svg = svg.replace('</svg>', circles + '</svg>');
+
+			var div = document.createElement('div');
+			div.innerHTML = svg;
+			this.imagesWrapper.appendChild(div);
+
 		}
 
 	};
