0451b5da14e0540879a2c4a0ccb5de4f98ad5103
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-02-03 18:37:52 +0300

    DRAW - in progress.

--- KidGameColoringBook/web/ios/js/controllers/draw.js
+++ KidGameColoringBook/web/ios/js/controllers/draw.js
@@ -5,13 +5,16 @@
 
 	var draw = {
 
-		start: function() {
+		start: function () {
+
+			ui.fn.setBodyScroll(true);
+			main.wrapper.removeAttribute('style');
 
 			var that = this;
 
 			// get figure SVG
 			var section = win.allFigures[info.currentSectionName];
-			section.forEach(function(obj){
+			section.forEach(function (obj) {
 				if (obj.id === info.currentImageId) {
 					that.svg = obj.svg;
 				}
@@ -21,10 +24,52 @@
 			var tempNode = document.createElement('div');
 			tempNode.innerHTML = this.svg;
 			var svgNode = $('svg', tempNode);
+			svgNode.setAttribute('class', 'js-main-svg main-svg');
 			var page = $('.page', main.wrapper);
 			page.appendChild(svgNode);
+			this.svgNode = svgNode;
+
+			// get image property
+			this.image = {};
+			this.image.width = parseInt(svgNode.getAttribute('width'), 10);
+			this.image.height = parseInt(svgNode.getAttribute('height'), 10);
+
+			this.image.scale = 1;
+			this.image.currentWidth = this.image.width;
+			this.image.currentHeight = this.image.height;
+
+			this.svgNode.style.width = this.image.width + 'px';
+			this.svgNode.style.height = this.image.height + 'px';
+
+			this.setScaleButtons();
+
+		},
+		setScaleButtons: function () {
+
+			var buttonPlus = $('.js-scale-button-plus', main.wrapper);
+			var buttonMinus = $('.js-scale-button-minus', main.wrapper);
+
+			buttonPlus.addEventListener('click', this.scaleImageBy.bind(this, 1.1), false);
+			buttonMinus.addEventListener('click', this.scaleImageBy.bind(this, 0.9), false);
+
+		},
+		scaleImageBy: function(q) {
+
+			if ((this.image.currentWidth < 100) || (this.image.currentHeight < 100)) {
+				return false;
+			}
+
+			this.image.scale *= q;
+			this.image.currentWidth *= q;
+			this.image.currentHeight *= q;
+
+			this.svgNode.style.width = this.image.currentWidth + 'px';
+			this.svgNode.style.height = this.image.currentHeight + 'px';
+
+			return true;
 
 		}
+
 
 	};
 
