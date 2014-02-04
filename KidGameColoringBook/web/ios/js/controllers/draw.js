24aed55c55a74611055720140e428bef25c762a9
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-02-04 21:06:04 +0300

    Draw - in progress.

--- KidGameColoringBook/web/ios/js/controllers/draw.js
+++ KidGameColoringBook/web/ios/js/controllers/draw.js
@@ -8,13 +8,93 @@
 		getPathSize: function (x0, y0, x1, y1) {
 			return Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
 		},
-		wasClick: function() {
+		wasClick: function () {
 			var x1, y1, x2, y2;
 			x1 = info.evt.touchStart.x;
 			y1 = info.evt.touchStart.y;
 			x2 = info.evt.touchMove.x;
 			y2 = info.evt.touchMove.y;
 			return utils.getPathSize(x1, y1, x2, y2) < this.minMove;
+		},
+		arrayToColor: function (arr) {
+			return 'rgb(' + arr.join(',') + ')';
+		}
+
+	};
+
+	var colorPicker = {
+		newColor: [255, 255, 255],
+		oldColor: [255, 255, 255],
+		currentColor: [255, 255, 255],
+		mainColors: [
+			[0, 0, 0], // black
+			[255, 0, 0], // red
+			[255, 255, 0], // yellow
+			[0, 255, 0], // green
+			[0, 255, 255], // light blue
+			[0, 0, 255], // blue
+			[255, 0, 255], // magenta
+			[255, 0, 0], // red
+			[255, 255, 255] // white
+		],
+		init: function () {
+			this.mainColorNode = $('.js-color-picker-main-color', main.wrapper);
+			this.secondaryColorNode = $('.js-color-picker-secondary-color', main.wrapper);
+			this.fadeNode = $('.js-color-picker-fade', main.wrapper);
+			this.newColorButton = $('.js-color-picker-new-color', main.wrapper);
+			this.oldColorButton = $('.js-color-picker-old-color', main.wrapper);
+
+			this.createColorMap();
+			this.setButtonsColor();
+			this.setMainColorPicker();
+		},
+		setButtonsColor: function () {
+			this.newColorButton.style.backgroundColor = utils.arrayToColor(this.newColor);
+			this.oldColorButton.style.backgroundColor = utils.arrayToColor(this.oldColor);
+		},
+		createColorMap: function () {
+			this.colorMap = [];
+			var that = this;
+			this.mainColors.forEach(function (color, index, arr) {
+				var percent = index / (arr.length - 1);
+				that.colorMap.push({percent: percent, color: color});
+			});
+			this.colorMap = this.colorMap.sort(function(a, b) {
+				return a.percent - b.percent;
+			});
+
+		},
+		setMainColorPicker: function () {
+
+			// set bgi
+			var bgi = 'left';
+			this.colorMap.forEach(function(color){
+				bgi += ', ' + utils.arrayToColor(color.color) + ' ' + (color.percent * 100) + '%'
+			});
+			bgi = info.preCSS + 'linear-gradient(' + bgi + ')';
+			this.mainColorNode.style.backgroundImage = bgi;
+
+			// add event listener
+			function getColorValue(e) {
+				// get left offset
+				var fillWidth = info.screen.getWidth();
+				var leftOffset = fillWidth * 0.05 + 5;
+				var fieldSize = fillWidth - leftOffset * 2;
+				var x = info.isTouch ? e.touches[0].pageX : e.pageX;
+				x -= leftOffset;
+				var percent = x / fieldSize;
+				percent = percent < 0 ? 0 : percent;
+				percent = percent > 1 ? 1 : percent;
+				console.log(percent);
+			}
+
+			this.mainColorNode.addEventListener(info.evt.down, getColorValue, false);
+			this.mainColorNode.addEventListener(info.evt.up, getColorValue, false);
+
+			if (info.isTouch) {
+				this.mainColorNode.addEventListener(info.evt.move, getColorValue, false);
+			}
+			
 		}
 
 	};
@@ -22,6 +102,7 @@
 	var draw = {
 		activeColor: '#0c0',
 		usedColors: [],
+		activeTool: 'brush',  // brush or picker
 		start: function () {
 
 			ui.fn.setBodyScroll(true);
@@ -61,6 +142,7 @@
 			this.setScaleButtons();
 			this.setSVGColoring();
 			this.setShowColorPickerButton();
+			colorPicker.init();
 
 		},
 		setScaleButtons: function () {
@@ -77,7 +159,9 @@
 			var that = this;
 
 			function setColorClick() {
-				this.setAttribute('fill', that.activeColor);
+				if (that.activeTool === 'brush') {
+					this.setAttribute('fill', that.activeColor);
+				}
 			}
 
 			function setColorTouchStart() {
@@ -85,7 +169,7 @@
 			}
 
 			function setColorTouchEnd() {
-				if (that.activePolygon === this && utils.wasClick()) {
+				if (that.activePolygon === this && utils.wasClick() && that.activeTool === 'brush') {
 					this.setAttribute('fill', that.activeColor);
 				}
 			}
@@ -134,16 +218,16 @@
 			var that = this;
 
 			if (info.isTouch) {
-				button.addEventListener(info.evt.down, function() {
+				button.addEventListener(info.evt.down, function () {
 					that.activePolygon = this;
 				}, false);
-				button.addEventListener(info.evt.up, function(){
+				button.addEventListener(info.evt.up, function () {
 					if (that.activePolygon === this && utils.wasClick()) {
 						that.showColorPickerTurn(true);
 					}
 				}, false);
 			} else {
-				button.addEventListener('click', function() {
+				button.addEventListener('click', function () {
 					that.showColorPickerTurn(true);
 				}, false);
 			}
