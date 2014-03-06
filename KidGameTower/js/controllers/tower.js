f522bf8e6a36ffac3638f97d41993a6b56356f8e
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-03-06 19:49:00 +0300

    KGTower - in progress.

--- KidGameTower/js/controllers/tower.js
+++ KidGameTower/js/controllers/tower.js
@@ -1,7 +1,7 @@
 (function (win, doc, docElem) {
 
 	"use strict";
-	/*global window, document, console, alert */
+	/*global window, document, console, alert, $, setTimeout, info, main */
 
 	var tower;
 
@@ -63,40 +63,56 @@
 			key;
 		for (key in blocks) {
 			if (blocks.hasOwnProperty(key)) {
-				if (this.coordinates.col === blocks[key].coordinates.col && this.coordinates.row === blocks[key].coordinates.row && this.id !== blocks[key].id) {
+				if (this.speed.row && this.coordinates.col === blocks[key].coordinates.col && this.coordinates.row === blocks[key].coordinates.row && this.id !== blocks[key].id) {
 					this.coordinates.row -= this.speed.row;
+					this.isActive = false;
 					break;
 				}
 			}
 		}
 
+//		if (this.coordinates.row === 0 && this.isActive === false)
+		if (!this.coordinates.row && !this.isActive) {
+			this.remove();
+		}
+
+	};
+
+	Block.prototype.remove = function() {
+		delete this.constructor.prototype.blockMap[this.id];
 	};
 
 	tower = {
 		field: {
 			size: {
 				width: 7,
-				height: 10
+				height: 14
 			}
+		},
+		cell: {
+			size: 20
 		},
 		isActive: false,
 		speed: 200,
 		symbols: {
 			full: '<i><\/i>',
-			empty: '<b><\/b>'
+			empty: '<b><\/b>',
+			inActiveFull: '<em><\/em>'
 		},
 		rows: {
 			created: 0
 		},
+		blockInLine: 3,
 		handleEvent: function() {
 
 		},
 		startGame: function() {
 			this.wrapperNode = $('.game-wrapper', main.wrapper);
+			this.wrapperNode.style.width = this.field.size.width * this.cell.size + 'px';
 			this.wrapperNode.addEventListener(info.evt.down, this.dropLine.bind(this), false);
-			this.createLine();
 			this.isActive = true;
 			this.createMatrix();
+			this.createLine();
 			setTimeout(this.step.bind(this), this.speed);
 		},
 		step: function() {
@@ -106,38 +122,53 @@
 			}
 
 			var blocks = Block.prototype.blockMap,
-				key;
+				key,
+				activeBlockCounter = 0;
 			for (key in blocks) {
-				if (blocks.hasOwnProperty(key) && blocks[key].isActive) {
-					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.empty;
+				if (blocks.hasOwnProperty(key)) {
+					if (blocks[key].isActive) {
+						this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.empty;
+					} else {
+						this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.inActiveFull;
+					}
 					blocks[key].update();
 				}
 			}
 
 			for (key in blocks) {
 				if (blocks.hasOwnProperty(key)) {
-					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.full;
+					if (blocks[key].isActive) {
+						this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.full;
+						activeBlockCounter += 1;
+					} else {
+						this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.inActiveFull;
+					}
 				}
 			}
 
-			this.wrapperNode.innerHTML = this.matrix.join('<br>').replace(/,/gi, ' ');
+			if (!activeBlockCounter) {
+				this.createLine();
+			}
+
+			this.wrapperNode.innerHTML = this.matrix.join('').replace(/,/gi, '');
 
 			setTimeout(this.step.bind(this), this.speed);
 
 		},
 		createMatrix: function() {
 
-			this.matrix = [];
+			var i, j, key, blocks;
 
-			for (var i = 0; i < this.field.size.height; i += 1) {
+			this.matrix = [];
+			for (i = 0; i < this.field.size.height; i += 1) {
 				this.matrix[i] = [];
-				for (var j = 0; j < this.field.size.width; j += 1) {
+				for (j = 0; j < this.field.size.width; j += 1) {
 					this.matrix[i][j] = this.symbols.empty;
 				}
 			}
 
-			var blocks = Block.prototype.blockMap;
-			for (var key in blocks) {
+			blocks = Block.prototype.blockMap;
+			for (key in blocks) {
 				if (blocks.hasOwnProperty(key)) {
 					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.full;
 				}
@@ -147,24 +178,47 @@
 
 		},
 		dropLine: function() {
-			var blocks = Block.prototype.blockMap;
-			for (var key in blocks) {
+			var blocks, key;
+			blocks = Block.prototype.blockMap;
+			for (key in blocks) {
 				if (blocks.hasOwnProperty(key) && blocks[key].isActive) {
 					blocks[key].speed = {
 						row: 1,
 						col: 0
-					}
+					};
 				}
 			}
-
-			this.createLine();
-
 		},
 		createLine: function() {
-			new Block(0, 2);
-			new Block(0, 3);
-			new Block(0, 4);
+
+			var that, minLineLength, centerCol, i;
+			that = this;
+
+			minLineLength = 0;
+
+			// detect line size
+			this.matrix.forEach(function(row){
+
+				if (minLineLength || row.indexOf(that.symbols.inActiveFull) === -1) {
+					return;
+				}
+
+				row.forEach(function(value) {
+					minLineLength += +(value === that.symbols.inActiveFull);
+				});
+
+			});
+
+			centerCol = Math.round((this.field.size.width - this.blockInLine) / 2) + 1;
+
+			for (i = 0; i < (this.blockInLine || minLineLength); i += 1) {
+				new Block(0, centerCol - i);
+			}
+
 			this.rows.created += 1;
+
+			this.blockInLine = 0;
+
 		}
 
 	};
