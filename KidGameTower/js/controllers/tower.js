4ae8cabee8852999e26687486507ec3dba03b51f
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-03-06 16:32:29 +0300

    KGTower - started.

---
+++ KidGameTower/js/controllers/tower.js
@@ -0,0 +1,158 @@
+(function (win, doc, docElem) {
+
+	"use strict";
+	/*global window, document, console, alert */
+
+	var tower;
+
+	function Block(row, col) {
+
+		this.isActive = true;
+
+		this.coordinates = {
+			row: row || 0,
+			col: col || 0
+		};
+
+		this.speed = {
+			row: 0,
+			col: 1
+		};
+
+		this.id = this.constructor.prototype.curreentId ? this.constructor.prototype.curreentId + 1 : 1;
+		this.constructor.prototype.curreentId = this.id;
+
+		// block add to blockMap
+		this.constructor.prototype.blockMap[this.id] = this;
+
+	}
+
+	Block.prototype.blockMap = {};
+
+	Block.prototype.update = function() {
+
+		if (!this.isActive) {
+			return;
+		}
+
+		this.coordinates.row += this.speed.row;
+		this.coordinates.col += this.speed.col;
+
+		if (this.coordinates.col > tower.field.size.width - 1) {
+			this.coordinates.col = tower.field.size.width - 1;
+			this.speed.col = -this.speed.col;
+		}
+
+		if (this.coordinates.row > tower.field.size.height - 1) {
+			this.coordinates.row = tower.field.size.height - 1;
+			this.isActive = false;
+		}
+
+		if (this.coordinates.row < 0) {
+			this.coordinates.row = 0;
+		}
+
+		if (this.coordinates.col < 0) {
+			this.coordinates.col = 0;
+			this.speed.col = -this.speed.col;
+		}
+
+	};
+
+	tower = {
+		field: {
+			size: {
+				width: 7,
+				height: 10
+			}
+		},
+		isActive: false,
+		speed: 500,
+		symbols: {
+			full: 1,
+			empty: 0
+		},
+		handleEvent: function() {
+
+		},
+		startGame: function() {
+			this.wrapperNode = $('.game-wrapper', main.wrapper);
+			this.wrapperNode.addEventListener(info.evt.down, this.dropLine.bind(this), false);
+			this.createLine();
+			this.isActive = true;
+			this.createMatrix();
+			setTimeout(this.step.bind(this), this.speed);
+		},
+		step: function() {
+
+			if (!this.isActive) {
+				return;
+			}
+
+			var blocks = Block.prototype.blockMap,
+				key;
+			for (key in blocks) {
+				if (blocks.hasOwnProperty(key) && blocks[key].isActive) {
+					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.empty;
+					blocks[key].update();
+				}
+			}
+
+			for (key in blocks) {
+				if (blocks.hasOwnProperty(key) && blocks[key].isActive) {
+					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.full;
+				}
+			}
+
+			this.wrapperNode.innerHTML = this.matrix.join('<br>').replace(/,/gi, ' ');
+
+			setTimeout(this.step.bind(this), this.speed);
+
+		},
+		createMatrix: function() {
+
+			this.matrix = [];
+
+			for (var i = 0; i < this.field.size.height; i += 1) {
+				this.matrix[i] = [];
+				for (var j = 0; j < this.field.size.width; j += 1) {
+					this.matrix[i][j] = this.symbols.empty;
+				}
+			}
+
+			var blocks = Block.prototype.blockMap;
+			for (var key in blocks) {
+				if (blocks.hasOwnProperty(key)) {
+					this.matrix[blocks[key].coordinates.row][blocks[key].coordinates.col] = this.symbols.full;
+				}
+			}
+
+			this.wrapperNode.innerHTML = this.matrix.join('<br>').replace(/,/gi, ' ');
+
+		},
+		dropLine: function() {
+			var blocks = Block.prototype.blockMap;
+			for (var key in blocks) {
+				if (blocks.hasOwnProperty(key) && blocks[key].isActive) {
+					blocks[key].speed = {
+						row: 1,
+						col: 0
+					}
+				}
+			}
+
+			this.createLine();
+
+		},
+		createLine: function() {
+			new Block(0, 2);
+			new Block(0, 3);
+			new Block(0, 4);
+		}
+
+
+	};
+
+	win.tower = tower;
+
+}(window, document, document.documentElement));
---
+++ KidGameTower/js/main.js
@@ -0,0 +1,36 @@
+(function (win) {
+
+	"use strict";
+	/*global window, document */
+
+	var main = {
+
+		handleEvent: function() {
+
+			this.wrapper = $('#wrapper');
+			viewer.show('title-page');
+			ui.fn.setBodyScroll(true);
+
+		},
+		setMorePage: function(){
+			var langSelectNode = $('.js-language-select', this.wrapper);
+
+			langSelectNode.addEventListener('change', function(){
+				info.set('lang', this.value, true);
+				viewer.refresh();
+			}, false);
+
+			// set external link
+			var links = $$('a', main.wrapper);
+			ui.externalLinkHandler.setLinks(links);
+
+		}
+
+	};
+
+	win.main = main;
+
+	win.addEventListener('load', main, false);
+
+}(window));
+
