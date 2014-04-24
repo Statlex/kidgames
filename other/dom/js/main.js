5c625fbacca61b4b5afcea9ebfec3da238b8b9d0
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-04-25 02:08:44 +0300

    DOM LIB - started.

---
+++ other/dom/js/main.js
@@ -0,0 +1,12 @@
+(function (win, doc, docElem) {
+
+	"use strict";
+	/*global console, alert */
+
+	console.log($('div').red('s'));
+
+//	console.log(bro.bar);
+
+
+
+}(window, document, document.documentElement));
