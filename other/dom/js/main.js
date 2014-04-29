35420894725abcda7fb4f44216f64f7e2498329e
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-04-29 18:04:16 +0300

    DOM - in progress.

--- other/dom/js/main.js
+++ other/dom/js/main.js
@@ -3,23 +3,12 @@
 	"use strict";
 	/*global console, alert */
 
-	var t = bro(document.querySelector('div'), {rr:'eeererer'});
-	console.log(t);
-
-	t = bro('body').append( bro('<div><img src="" alt=""/></div>', {rr:'eeererer'}));
-	console.log(t);
 
 
-	t = bro('<div/>', {test:45677}).appendTo(document.body).addClass('test');
-	console.log(t);
-
-	bro('div').on('click', 'span', function(){
-		console.log(this);
-		console.log(event.data);
-		console.log('clicked');
-	});
-
-	bro('div span').on('click');
+	console.log($('body').css({
+		top: '30px',
+		position: 'fixed'
+	}));
 
 //	console.log(bro('div').find('span').remove());
 
