dcc831df6c098730dd0eef56ac56401fd89aafa3
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-05-16 01:19:31 -0700

    KGT - in progress.

--- KidGameTower/web/js/tower.js
+++ KidGameTower/web/js/tower.js
@@ -12,16 +12,18 @@
 	info = {
 		squareSize: 20,
 		w: 7,
-		h: 14
+		h: 14,
+		maxFloor: 10
 	};
 
 	tower = {
+		count: -1, // see lineLength
 		wrapper: 0, // node wrapper
 		blocks: [],
 		isActive: true,
 		lineLength: 3, // must be
 		defaultLineLength: 3, // the same
-		stepTime: 300,
+		stepTime: 220,
 		init: function() {
 			this.wrapper.on('click', this.dropBlocks.bind(this));
 		},
@@ -47,10 +49,10 @@
 			});
 
 		},
-		createBlock: function() {
+		createBlock: function(direction) {
 
 			var newBlock = Object.create(block);
-			newBlock.init();
+			newBlock.init(direction);
 
 			this.wrapper.append(newBlock.node);
 			this.blocks.push(newBlock);
@@ -60,57 +62,73 @@
 		},
 		createLine: function() {
 
-			var centerX = Math.round(info.w + 0.5) / 2,
+			var centerX = Math.round(info.w - 1.5) / 2,
+				direction = Math.random() > 0.5 ? 1 : -1,
 				block;
 
 			switch (this.lineLength) {
 
 				case 1:
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX;
 					break;
 
 				case 2:
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX;
 
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX + 1;
 					break;
 
 				case 3:
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX;
 
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX + 1;
 
-					block = this.createBlock();
+					block = this.createBlock(direction);
 					block.tower = this;
 					block.x = centerX - 1;
 					break;
 
 				default : // detect 0 and other
-					console.log('end the game');
+					console.log('end');
+					this.isActive = false;
 					break;
 
 			}
-
-		},
-		step: function() {
 
 			if (!this.isActive) {
 				return;
 			}
 
+			this.stepTime *= 0.99;
+			this.count += 1;
+
+			bro('.js-count').html(this.count);
+
+		},
+		step: function() {
+
+			if (!this.isActive) {
+				this.endGame();
+				return;
+			}
+
 			// remove extra block 1110111
 			var bottomBlockCount = 0,
-				createLine = true;
+				createLine = true,
+				maxFloor = info.h,
+				removeBlock,
+				ii, len,
+				shiftCount = 0;
 
 			this.blocks.forEach(function(block){
 				if (block.y === info.h - 1) {
@@ -120,6 +138,9 @@
 
 			if (bottomBlockCount === this.defaultLineLength * 2) {
 				console.log('end');
+				this.isActive = false;
+				this.endGame();
+				return;
 			}
 
 			this.blocks.forEach(function(block){
@@ -139,7 +160,36 @@
 			});
 
 			if (createLine) {
+				// try to remove extra floors
+				this.blocks.forEach(function(block){
+					if (block.direction === 0 && block.g === 0) {
+						maxFloor = Math.min(block.y, maxFloor);
+					}
+				});
+
+				if (maxFloor !== info.h && maxFloor < info.maxFloor) {
+					// down 3 blocks
+
+					this.blocks.forEach(function(block){
+						if (!block.direction && !block.g && block.y === info.h - 1) {
+							shiftCount += 1;
+						}
+					});
+
+					for (ii = 0, len = shiftCount; ii < len; ii += 1) {
+						removeBlock = this.blocks.shift();
+						removeBlock.killBlock();
+					}
+
+					this.blocks.forEach(function(block){
+						block.y += 1;
+						block.node.style[jsPre + 'Transition'] = 'all ease-out 0.3s';
+						block.node.style[jsPre + 'Transform'] = 'translate(' + block.x * info.squareSize +  'px, ' + block.y * info.squareSize + 'px)';
+					});
+				}
+
 				this.createLine();
+
 			}
 
 			this.blocks.forEach(function(block, index, arr){
@@ -178,15 +228,23 @@
 
 			setTimeout(this.step.bind(this), this.stepTime);
 
+		},
+		endGame: function() {
+			alert('your count is: ' + this.count);
+			alert('end game, to restart refresh page');
+
 		}
 	};
 
 	block = {
 		x: 0,
 		y: 0,
-		init: function() {
+		direction: 1, // 1 to right, -1 to left
+		g: 0,
+		init: function(direction) {
 			this.node = bro('<div class="block"/>')[0];
 			this.timeStamp = Date.now();
+			this.direction = direction || 1;
 		},
 		step: function() {
 
@@ -261,13 +319,14 @@
 
 		},
 		removeBlock: function() {
-			this.node.parentNode.removeChild(this.node);
 			this.tower.lineLength -= 1;
+			this.killBlock();
+		},
+		killBlock: function() {
+			this.node.parentNode.removeChild(this.node);
 			delete this.tower;
 			delete this.node;
-		},
-		direction: 1, // 1 to right, -1 to left
-		g: 0
+		}
 	};
 
 	// run game
