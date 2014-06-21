83b5b2fc48f8a7f79185c740510f1540d152696e
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-06-21 00:40:57 -0700

    KGB - in progress.

--- KGBingo/www/js/object/bingo/bingo.js
+++ KGBingo/www/js/object/bingo/bingo.js
@@ -10,8 +10,11 @@
 		templates: ['bingo'],
 		events: {
 
+			'click .js-bingo-word': 'setWord'
 
 
+		},
+		selectors: {
 
 		},
 		init: function() {
@@ -31,6 +34,58 @@
 		},
 		copyObject: function(obj) {
 			return JSON.parse(JSON.stringify(obj));
+		},
+		setWord: function(e) {
+			var $node = $(e.currentTarget);
+
+			if ($node.data('checked') === 'true') {
+				$node.data('checked', 'false');
+				$node.removeClass('checked');
+			} else {
+				$node.data('checked', 'true');
+				$node.addClass('checked');
+			}
+
+			this.bingoTest();
+
+		},
+		bingoTest: function() {
+			var table = this.$el.find('.js-words-tablet'),
+				i, diagonal_1 = [], diagonal_2 = [], $nodes, isBingo, $row, $column, diagonalNode;
+
+			for (i = 1; i <= 5; i += 1) {
+
+				// get diagonal data
+				diagonalNode = this.$el.find('.js-words-tablet .table-row:nth-child(' + i + ') .table-cell:nth-child(' + i + ') [data-checked="true"]');
+				if (!diagonalNode.isEmpty()) {
+					diagonal_1.push(diagonalNode);
+				}
+
+				diagonalNode = this.$el.find('.js-words-tablet .table-row:nth-child(' + i + ') .table-cell:nth-child(' + (6 - i) + ') [data-checked="true"]');
+				if (!diagonalNode.isEmpty()) {
+					diagonal_2.push(diagonalNode);
+				}
+
+				// horizontal test
+				$row = this.$el.find('.js-words-tablet .table-row:nth-child(' + i + ')');
+				$nodes = $row.find('[data-checked="true"]');
+
+				isBingo = isBingo || $nodes.length === 5;
+
+				// vertical test
+				$column = this.$el.find('.js-words-tablet .table-row .table-cell:nth-child(' + i + ')');
+				$nodes = $column.find('[data-checked="true"]');
+
+				isBingo = isBingo || $nodes.length === 5;
+
+				// test diagonal
+				isBingo = isBingo || diagonal_1.length === 5;
+				isBingo = isBingo || diagonal_2.length === 5;
+
+			}
+
+			console.log(isBingo);
+
 		}
 
 
