4300f09da9f38c74ad1323dae7950c9a4fd19ec2
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-06-25 23:04:30 -0700

    SXLogic -start.

---
+++ SXlogic/www/js/question/section.js
@@ -0,0 +1,24 @@
+(function (win) {
+
+	"use strict";
+	/*global window */
+
+	win.sections = win.sections || {};
+
+	win.sections.test = {
+		name: 'тестовая секция',
+		items: [
+			{
+				name: 'Прошлогодний снег',
+				image: '',
+				text: 'Как найти прошлогодний снег?',
+				answer: 'Выйти на улицу сразу после начала нового года.'
+			}
+
+
+
+		]
+
+	};
+
+}(window));
