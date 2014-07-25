e76aee82cecb4917e11c2eafa282e7844d1751f5
Dmitriy Turovtsov <dmitriy.turovtsov@viaden.com>
2014-07-25 19:00:30 +0300

    SXLogic - rate us - in progress.

--- SXlogic/www/js/object/rate-us-view.js
+++ SXlogic/www/js/object/rate-us-view.js
@@ -6,30 +6,47 @@
 
 	win.APP = win.APP || {};
 
-	win.APP.QuestionView = win.APP.MainView.extend({
+	win.APP.RateUsView = win.APP.MainView.extend({
 		templates: ['rate-us'],
 		events: {
 //			'click .js-set-question': 'setQuestion',
 //			'click .js-hint-button': 'showHint',
 			'click h1': 'test'
 		},
+		period: 1, // one day
+		oneDayMs: 1000 * 60 * 60 * 24,
+		parent: '.js-wrapper',
 		init: function (data) {
 
-			this.interval = data.interval || 15;
-
-			this.timeOutIntervalId = win.setInterval((function(){
-				this.show();
-				console.log(this.tmpl);
-				clearInterval(this.timeOutIntervalId);
-			}.bind(this)), this.interval * 1000); // every 20 min
-
 			this.$el = $(this.tmpl['rate-us']({}));
+
+			this.timeOutIntervalId = win.setInterval(this.show.bind(this), 1000);
 
 		},
 		show: function() {
 
+			if (this.isShow) {
+				return;
+			}
+
+			var now = Date.now(),
+				lastShow = info.get('last-show-rate-us');
+
+			if ( now < (lastShow + this.period * this.oneDayMs * 0.00000000001 ) ) {
+				return;
+			}
+
+			this.isShow = true;
 			$(this.parent).append(this.$el);
 
+			info.set('last-show-rate-us', now, true);
+
+			clearInterval(this.timeOutIntervalId);
+
+		},
+		remove: function() {
+			$('.js-rate-us-wrapper').remove();
+			clearInterval(this.timeOutIntervalId);
 		},
 		test: function() {
 			console.log('test');
@@ -40,28 +57,34 @@
 
 	win.addEventListener('load', function(){
 
+		// <!-- get data of first run
+		if ( info.get('was-rated') ) {
+			return;
+		}
+
+		if ( !info.get('last-show-rate-us') ) {
+			info.set('last-show-rate-us', Date.now(), true);
+		}
+		// --> get data of first run
 
 		setTimeout(function(){
-			// <!-- get data of first run
-			if (info.get('was-rated')) {
+
+			win.APP.rateUsView = new win.APP.RateUsView();
+
+		}, 2000);
+
+		function hideRateUs() {
+
+			if (!win.APP.rateUsView) {
 				return;
 			}
 
-			if (!info.get('first-run')) {
-				info.set('first-run', Date.now(), true);
-			}
-			// --> get data of first run
+			win.APP.rateUsView.remove();
+			delete win.APP.rateUsView;
 
+		}
 
-			win.APP.questionView = new win.APP.QuestionView({
-				date: Date.now(),
-				url: '#question',
-				interval: 0.1,
-				parent: '.js-wrapper'
-			});
-
-		}, 500);
-
+		win.addEventListener('hashchange', hideRateUs, false);
 
 	}, false);
 
