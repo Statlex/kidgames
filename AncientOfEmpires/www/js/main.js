2ff1997bc7f34b08880b6bd89e9bb61fb9d5a8ff
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-10-09 21:42:11 +0300

    Revert "FAF - init."
    This reverts commit 5cbd76fd7c442a13d398c28be87c2c587eedd1e1.

---
+++ AncientOfEmpires/www/js/main.js
@@ -0,0 +1,81 @@
+(function (win, doc) {
+
+	"use strict";
+	/*global window, document */
+	/*global templateMaster, lang, Backbone, APP, info, history, setTimeout, $ */
+
+	lang.push('en');
+
+	window.APP = window.APP || {};
+
+	var Router;
+
+	Router = Backbone.Router.extend({
+
+		routes: {
+			'': 'title',
+			'battle': 'battle',
+			'store': 'store',
+			'maps': 'maps',
+			'create-map': 'createMap'
+		},
+
+		title: function () {
+
+			APP.titleView = new APP.TitleView({ currentView: APP.titleView });
+
+		},
+
+		battle: function () {
+
+			$('.js-wrapper .js-status-bar').data('state', 'battle');
+			APP.removeExtraView();
+
+		},
+
+		store: function () {
+
+			$('.js-wrapper .js-status-bar').data('state', 'store');
+
+		},
+
+		maps: function() {
+
+			APP.mapsView = new APP.MapsView();
+
+		},
+
+		createMap: function() {
+
+			new APP.CreateMapView();
+
+		}
+
+	});
+
+	APP.router = new Router();
+
+	APP.removeExtraView = function() {
+		$('.js-store-wrapper').remove();
+	};
+
+	// start of app here
+	function main() {
+		templateMaster.init();
+		Backbone.history.start();
+
+		function back() {
+			if (win.location.hash) {
+				history.back();
+				setTimeout(back, 200);
+			}
+		}
+
+		back();
+	}
+
+	win.addEventListener('load', main, false);
+
+	// other data here
+
+}(window, document));
