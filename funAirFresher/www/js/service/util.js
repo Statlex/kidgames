7a0fb1fe99c92abf88459e8fe8d30f908a8d64f1
Dmitriy Turovtsov <dmitriy.turovtsov@viaden.com>
2014-10-13 11:10:16 +0300

    FAF - I

--- funAirFresher/www/js/service/util.js
+++ funAirFresher/www/js/service/util.js
@@ -9,8 +9,10 @@
 	APP.util = {
 		setWrapperStyle: function(node) {
 
+			console.log('!!!! --- 181760 - get Math.pow(0.5) !!!!!!!');
+
 			var screenSize = docElem.clientWidth * docElem.clientHeight,
-				fontSize = Math.round(16 * screenSize / 181760);
+				fontSize = Math.round(16 * screenSize / 181760 );
 
 			node.css({
 				'font-size': fontSize + 'px'
