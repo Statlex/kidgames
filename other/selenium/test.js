edf3085915014160d0c111bdf77364fd450adc0b
DmitriTurovtsov <Dmitri.Turovtsov@nasty-creatures.com>
2014-10-26 16:38:46 +0300

    SE - init.

--- other/selenium/test.js
+++ other/selenium/test.js
@@ -3,25 +3,39 @@
 	"use strict";
 	/*global */
 
+	process.argv.forEach(function (val, index, array) {
+		console.log(index + ': ' + val);
+	});
+
 	console.log('//');
+	console.log(arguments);
 
 	var webdriver = require('selenium-webdriver');
 
+	var SELENIUM_HOST = 'http://localhost:8080/wd/hub';
+
 	var driver = new webdriver.Builder().
+		usingServer(SELENIUM_HOST).
 		withCapabilities(webdriver.Capabilities.chrome()).
+//		withCapabilities({ browserName: 'firefox' }).
 		build();
 
-	driver.get('http://www.google.com');
-	var promise = driver.getTitle();
+	driver.get("http://statlex.com/game/air/");
 
-	promise.then(function(title) {
-		console.log("title is: " + title);
-	});
+//	driver.sleep(1000);
 
-	promise.then(function(title) {
-		console.log("title still is: " + contents);
-	});
+	driver.findElement(webdriver.By.css('[data-route="privacy-policy"]')).click()
+		.then(function(){
 
-	driver.quit();
+			var tt = driver.findElement(webdriver.By.css('.ui-corner-all'))
+				.getInnerHtml().then(function(html){
+					console.log(html);
+				});
+
+			console.log(tt);
+		});
+
+
+	//driver.quit();
 
 }());
